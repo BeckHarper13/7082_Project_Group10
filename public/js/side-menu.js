98cad5d166e44e47388494877ac599ad5c99e38f
Navjot Kehal <navjotkehal5@gmail.com>
2025-10-27 23:41:12 -0700

    Created a side menu for navigation between pages

---
+++ public/js/side-menu.js
@@ -0,0 +1,6 @@
+const container = document.getElementById("menuRoot");
+ReactDOM.createRoot(container).render(React.createElement(SideMenu));
+
+document.getElementById("logout").addEventListener('click', () => {
+    // call logout post function
+})
