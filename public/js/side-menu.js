dcad0ef2d56bade3e042ac4e3ca1ef5689c935b0
Navjot Kehal <navjotkehal5@gmail.com>
2025-10-29 16:52:20 -0700

    Deleted side-menu.js and converted the side menu buttons to anchor tags

--- public/js/side-menu.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("accountBtn").addEventListener("click", () => {
-    window.location.href = "/account";
-})
-document.getElementById("homeBtn").addEventListener("click", () => {
-    window.location.href = "/home";
-})
-document.getElementById("addCarBtn").addEventListener("click", () => {
-    window.location.href = "/cars";
-})
