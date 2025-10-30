c00df08e0f20080b3d56c562f893ab132cf4c79f
Timur Reziapov <timurreziapov2@gmail.com>
2025-10-30 11:11:13 -0700

    removes unused files

--- public/js/landing-page.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("login").addEventListener('click', () => {
-    window.location.href = "login.html";
-});
-
-document.getElementById("signup").addEventListener('click', () => {
-    window.location.href = "signup.html";
-});
